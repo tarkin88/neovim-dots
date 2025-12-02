786ea9287c3126c015626f830c23aba06d10fbeb
Francisco Suarez <franksg88@gmail.com>
2025-12-02 16:18:30 -0600

    feat(colorscheme): catppuccin added

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,47 @@
 return {
-  "rose-pine/neovim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   event = "VeryLazy",
-  name = "rose-pine",
-  config = function(_, opts) vim.cmd("colorscheme rose-pine-moon") end,
+  priority = 1000,
+  opts = {
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+      comments = { "italic" }, -- Change the style of comments
+      conditionals = { "italic" },
+      loops = {},
+      functions = { "bold" },
+      keywords = { "italic" },
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = { "underline" },
+      operators = { "italic" },
+      -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    },
+    lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+        ok = { "italic" },
+      },
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+        ok = { "underline" },
+      },
+      inlay_hints = {
+        background = false,
+      },
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+
+    vim.cmd.colorscheme("catppuccin-frappe")
+  end,
 }
