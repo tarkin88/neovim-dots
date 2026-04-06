4fedb87e59f0327e669e4b2db61a956de66a1d68
Francisco Suarez <franksg88@gmail.com>
2026-04-06 13:25:30 -0600

    chore(treesitter): remove vim-matchup plugin and related setup
    Remove andymass/vim-matchup from plugin dependencies.
    Remove match-up treesitter option and plugin setup call.
    Remove matchparen offscreen popup global setting.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-context",
-    "andymass/vim-matchup",
     "folke/ts-comments.nvim",
     "andersevenrud/nvim_context_vt",
   },
@@ -14,24 +13,15 @@ return {
       additional_vim_regex_highlighting = false,
     },
     indent = { enable = true },
-    matchup = { enable = true },
   },
   config = function(_, opts)
     require("nvim-treesitter.config").setup(opts)
-
-    require("match-up").setup({
-      treesitter = {
-        stopline = 500,
-      },
-    })
 
     require("treesitter-context").setup({
       enable = true,
       max_lines = 3,
       trim_scope = "outer",
     })
-
-    vim.g.matchup_matchparen_offscreen = { method = "popup" }
 
     require("nvim-treesitter").install({
       "bash",
